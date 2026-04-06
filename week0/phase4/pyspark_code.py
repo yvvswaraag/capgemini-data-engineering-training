f59785cb4936a1a2d09bde5cb145c296ac75dcb3
yvvswaraag <155514885+yvvswaraag@users.noreply.github.com>
2026-04-06 14:08:26 +0530

    Update pyspark_code.py

--- week0/phase4/pyspark_code.py
+++ week0/phase4/pyspark_code.py
@@ -11,6 +11,8 @@ print("1. Daily Sales")
 sales.groupBy("sale_date").sum("total_amount").withColumnRenamed("sum(total_amount)","Daily_sales").select("sale_date","Daily_sales").show()
 # Display the DataFrame using the display() function.
 display(df)
+
+
 print("2. City-wise Revenue")
 sales.join(customers,on="customer_id",how="inner").groupBy("city").sum("total_amount").withColumnRenamed("sum(total_amount)","total_revenue").select("city","total_revenue").show()
 
@@ -18,8 +20,11 @@ print("3. Top 5 Customers")
 from pyspark.sql.functions import col, sum, concat_ws
 df = df.withColumn("customer_name", concat_ws(" ", col("first_name"), col("last_name")))
 df.groupBy("customer_name").agg(sum("total_amount").alias("total_spend")).orderBy(col("total_spend").desc()).limit(5).show()
+
 print("4. Repeat Customers (>1 order)")
 sales.groupBy("customer_id").count().filter("count > 1").withColumnRenamed("count", "order_count").show()
+
+
 print("5.  Customer Segmentation → Create business logic: total_spend > 10000 → Gold 5000–10000 →Silver <5000 → Bronze")
 customer_spend = df.groupBy("customer_name").agg(sum("total_amount").alias("total_spend"))
 customer_segment = customer_spend.withColumn(
@@ -30,6 +35,8 @@ customer_segment = customer_spend.withColumn(
 )
 
 customer_segment.show()
+
+
 print("6. Final Reporting Table → Combine all relevant insights into a final table.")
 order_count = sales.groupBy("customer_id").agg(count("*").alias("order_count"))
 
